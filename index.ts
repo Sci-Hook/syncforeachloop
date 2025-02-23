97022ae039d7b2de31e6db57f9f0ef26b19434c0
Emirhan Gerçeker <emirhan@scihook.org>
2025-02-23 08:28:13 +0300

    feat: add finish function

--- index.ts
+++ index.ts
@@ -9,17 +9,47 @@ declare global {
 Array.prototype.syncForEach = function<T>(this: T[], callback: any,ending_function?:any): any {
     return new Promise<any>((resolve, reject) => {
         let index = -1;
+        let resolved = false
+
         const next = () => {
             index++;     
             if (this.length > index) {
                 if (this.length > 0) {
-                    callback(this[index], next,index+1,this.length);
+                    callback(this[index], next, index+1, this.length,finish);
                 }
             }else{
-                resolve(ending_function ? ending_function : true);
-                if (ending_function) ending_function();
+                if (!resolved) {
+                    resolve(ending_function ? ending_function : true);
+                    if (ending_function) ending_function();
+                }
             }
         }
+
+        const finish = () => {
+            resolved = true;
+            index = this.length;
+            resolve(ending_function ? ending_function : true);
+            if (ending_function) ending_function();
+        }
+
         next();
     });
 }
+
+
+let test = ['a','b','c','d'];
+
+async function main() {
+    await test.syncForEach(function (item,next,i,l,finish) {
+        console.log(item);
+        if (i == 2) {
+            finish();  
+        }
+        next();
+    },() => {
+        console.log('ben');
+    });
+}
+
+
+main();
